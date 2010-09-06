fefecec295865f6e33f31191d8a3b8ab3c1a71f3
Andrés Bonilla <andresbonilla@gmail.com>
2010-09-05 20:27:14 -0400

    some link changes

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -24,12 +24,22 @@ describe PagesController do
       get 'about'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", :content => @base_title+" | About")
+    end
   end
 
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", :content => @base_title+" | Contact")
     end
   end
 
@@ -38,6 +48,11 @@ describe PagesController do
       get 'help'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'help'
+      response.should have_selector("title", :content => @base_title+" | Help")
+    end
   end
 
   describe "GET 'terms'" do
@@ -45,12 +60,22 @@ describe PagesController do
       get 'terms'
       response.should be_success
     end
+    
+    it "should have the right title" do
+      get 'terms'
+      response.should have_selector("title", :content => @base_title+" | Terms")
+    end
   end
 
   describe "GET 'privacy'" do
     it "should be successful" do
       get 'privacy'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'privacy'
+      response.should have_selector("title", :content => @base_title+" | Privacy")
     end
   end
 
