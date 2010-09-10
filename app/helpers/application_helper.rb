08cd68d4c0054428510be6fb7a5a5fbb055cae94
Andrés Bonilla <andresbonilla@gmail.com>
2010-09-10 01:12:51 -0400

    CSS

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 
   # Return a title on a per-page basis.
   def title
-    base_title = "Password Bucket"
+    base_title = "loboto.me"
     if @title.nil?
       base_title
     else
