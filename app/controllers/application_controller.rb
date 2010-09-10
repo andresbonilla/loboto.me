f6046ed9e5e650c2ae8d8faf541c28faa0fcab81
Andrés Bonilla <andresbonilla@gmail.com>
2010-09-10 16:48:14 -0400

    removed default validation error html

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,4 +16,19 @@ class ApplicationController < ActionController::Base
   end
   
 
+
+
+
+  ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    error_class = "fieldError"
+    if html_tag =~ /<(input|textarea|select)[^>]+class=/
+      style_attribute = html_tag =~ /class=['"]/
+      html_tag.insert(style_attribute + 7, "#{error_class} ")
+    elsif html_tag =~ /<(input|textarea|select)/
+      first_whitespace = html_tag =~ /\s/
+      html_tag[first_whitespace] = " class='#{error_class}' "
+    end
+    html_tag
+  end
+
 end
